4d764089e143ef501dec61254a007add251ace4a
Dummy <onyourleftpal@gmail.com>
2024-09-19 13:47:59 +0300

    setup for view folder

--- app.js
+++ app.js
@@ -37,6 +37,8 @@ app.use((req, res, next) => {
 });
 
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'))
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
